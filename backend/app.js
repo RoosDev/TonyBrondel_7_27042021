0daf9d5a180d4c7b2cc0c92f287f80c36c68838e
Tony B <dev@difiworld.com>
2021-09-22 22:43:46 +0200

    Premier commit
    ** FRONT END **
    
    - vue 3 installé
    - le logo de la page home vue changé
    
    ** BACK END **
    
    - server.js réalisé avec condition https / http
    - trame de fichier app.js en PoolNamespace
    - packages installés
    - .env mise en place
    
    => server fonctionnel http sur port 3001
    (si https en place : port 3000)

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+    // import des extensions necessaires
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+
+// Définition des routes
+// const sauceRoutes = require("./routes/sauce_routes.js");
+// const userRoutes = require("./routes/user_routes");
+
+// Mise en place des variables d'environnement
+const dotenv = require("dotenv");
+dotenv.config();
+// const URL_MonDb = process.env.URL_DB;
+
+//paramétrage de la connexion à la DB MongoDB
+
+
+
+
+// instantiation express
+const app = express();
+
+// Sécurisation avec le package Helmet
+app.use(helmet());
+app.use(
+  helmet.frameguard({
+    action: "deny",
+  })
+);
+
+// Gestion des headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, "
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+// body parser est directement intégré à express maintenant
+app.use(express.json());
+
+// Appel des differents modules de l'app
+// app.use("/images", express.static(path.join(__dirname, "images")));
+// app.use("/api/sauces", sauceRoutes);
+// app.use("/api/auth", userRoutes);
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,77 @@
+// Chargement des packages
+
+const http = require("http");
+const https = require("https");
+const app = require("./app");
+const dotenv = require("dotenv");
+const fs = require('fs');
+
+// import du module
+// import { existsSync } from 'fs';
+
+// Chargement des variables d'environnement 
+dotenv.config();
+const APP_PORTS = process.env.PORTS;
+const APP_PORT = process.env.PORT;
+const KEY_HTTPS = process.env.Key_Path_HTTPS;
+const CERT_HTTPS = process.env.Cert_Path_HTTPS;
+
+// Test pour HTTPS ou HTTP
+if( fs.existsSync(KEY_HTTPS) && fs.existsSync(CERT_HTTPS) ){
+    const options = {
+        key: fs.readFileSync(KEY_HTTPS),
+        cert: fs.readFileSync(CERT_HTTPS)
+    };
+    
+    https.createServer(options, (req, res) => {
+        res.writeHead(200);
+        res.end('hello world\n Let\' go !!\n');
+    }).listen(APP_PORTS);
+}else{
+
+    const normalizePort = (val) => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+    };
+    const port = normalizePort(APP_PORT);
+    app.set("port", port);
+
+    const errorHandler = (error) => {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    const address = server.address();
+    const bind =
+        typeof address === "string" ? "pipe " + address : "port: " + port;
+    switch (error.code) {
+        case "EACCES":
+        console.error(bind + " requires elevated privileges.");
+        process.exit(1);
+        break;
+        case "EADDRINUSE":
+        console.error(bind + " is already in use.");
+        process.exit(1);
+        break;
+        default:
+        throw error;
+    }
+    };
+
+    const server = http.createServer(app);
+
+    server.on("error", errorHandler);
+    server.on("listening", () => {
+    const address = server.address();
+    const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+    console.log("Listening on " + bind);
+    });
+
+    server.listen(port);
+}
